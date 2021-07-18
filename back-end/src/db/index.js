ff6241916cb6419bfee08b8630456adb65e43c19
bellDev-code <sumaoo20@naver.com>
2021-07-18 16:15:42 +0900

    DB table create FK

--- back-end/src/db/index.js
+++ back-end/src/db/index.js
@@ -7,16 +7,35 @@ const db = require("mysql").createPool({
   password: process.env.password,
 });
 
+// ALTER 안써지는 문법 문제 물어보기
+
 db.getConnection((err, conn) => {
+  conn.query(
+    `  
+    CREATE TABLE IF NOT EXISTS TB_Manufacture
+    (
+        manufacture_id    INT            NOT NULL    AUTO_INCREMENT COMMENT '제품 제조사 아이디', 
+        manufacture_name  VARCHAR(45)    NULL        COMMENT '제조사 이름', 
+        manufacture_adr   VARCHAR(45)    NULL        COMMENT '제조사 주소', 
+        CONSTRAINT PK_TB_manufacture PRIMARY KEY (manufacture_id)
+    );
+    `,
+    (err, results, fields) => {
+      if (err) throw err;
+    }
+  );
+
   conn.query(
     `  
       CREATE TABLE IF NOT EXISTS TB_Info
       (
           info_id          INT            NOT NULL    AUTO_INCREMENT COMMENT '제품 정보 아이디', 
-          manufacturer_id  INT            NULL        COMMENT '제품 제조사 아이디', 
+          manufacture_id  INT            NULL        COMMENT '제품 제조사 아이디', 
           info_gender      ENUM('female', 'male')           NULL        COMMENT '제품 성별', 
           info_material    VARCHAR(45)    NULL        COMMENT '제품 소재', 
-          CONSTRAINT PK_TB_Info PRIMARY KEY (info_id)
+          CONSTRAINT PK_TB_Info PRIMARY KEY (info_id),
+          CONSTRAINT FK_TB_Manufacture_manufacture_id_TB_Info_manufacture_id FOREIGN KEY(manufacture_id)
+          REFERENCES TB_Manufacture(manufacture_id)
       );
     `,
     (err, results, fields) => {
@@ -43,6 +62,22 @@ db.getConnection((err, conn) => {
 
   conn.query(
     `  
+    CREATE TABLE IF NOT EXISTS TB_Certification
+    (
+        certificate_id      INT            NOT NULL    COMMENT '인증서 아이디', 
+        number_times  INT            NULL        COMMENT '대여 횟수', 
+        responsible   VARCHAR(45)    NULL        COMMENT '책임자', 
+        code          VARCHAR(45)    NULL        COMMENT '일련번호', 
+        CONSTRAINT PK_TB_certification PRIMARY KEY (certificate_id)
+    );
+    `,
+    (err, results, fields) => {
+      if (err) throw err;
+    }
+  );
+
+  conn.query(
+    `  
       CREATE TABLE IF NOT EXISTS TB_Products
       (
           product_id        INT            NOT NULL    AUTO_INCREMENT COMMENT '제품 아이디', 
@@ -53,27 +88,15 @@ db.getConnection((err, conn) => {
           rent_price        INT            NULL        COMMENT '렌트 가격', 
           product_state     VARCHAR(45)    NULL        COMMENT '제품 상태', 
           product_color     VARCHAR(45)    NULL        COMMENT '제품 색상', 
-          certification_id  INT            NULL        COMMENT '인증서 아이디', 
+          certificate_id  INT            NULL        COMMENT '인증서 아이디', 
           CONSTRAINT PK_TB_Products PRIMARY KEY (product_id),
           CONSTRAINT FK_TB_Info_info_id_TB_Products_info_id  FOREIGN KEY (info_id)
           REFERENCES TB_Info(info_id),
           CONSTRAINT FK_TB_ProductGroup_group_id_TB_Products_group_id  FOREIGN KEY (group_id)
-          REFERENCES TB_ProductGroup(group_id)
+          REFERENCES TB_ProductGroup(group_id),
+          CONSTRAINT FK_TB_Certification_certificate_id_TB_Products_certificate_id FOREIGN KEY (certificate_id)
+          REFERENCES TB_Certification(certificate_id)
       )
-    `,
-    (err, results, fields) => {
-      if (err) throw err;
-    }
-  );
-
-  conn.query(
-    `  
-    CREATE TABLE IF NOT EXISTS TB_Basket
-    (
-        basket_id   INT    NOT NULL    AUTO_INCREMENT COMMENT '장바구니 아이디', 
-        product_id  INT    NULL        COMMENT '제품 아이디', 
-        CONSTRAINT PK_TB_Basket PRIMARY KEY (basket_id)
-    );
     `,
     (err, results, fields) => {
       if (err) throw err;
@@ -90,7 +113,9 @@ db.getConnection((err, conn) => {
         end_rent     DATETIME    NULL        COMMENT '렌트 종료시간', 
         daily_price  INT         NULL        COMMENT '하루당 렌트 가격', 
         total_price  INT         NULL        COMMENT '총결제금액', 
-        CONSTRAINT PK_TB_Payment PRIMARY KEY (payment_id)
+        CONSTRAINT PK_TB_Payment PRIMARY KEY (payment_id),
+        CONSTRAINT FK_TB_Products_product_id_TB_Payment_product_id FOREIGN KEY(product_id)
+        REFERENCES TB_Products(product_id)
     );
     `,
     (err, results, fields) => {
@@ -100,13 +125,13 @@ db.getConnection((err, conn) => {
 
   conn.query(
     `  
-    CREATE TABLE IF NOT EXISTS TB_Certification
+    CREATE TABLE IF NOT EXISTS TB_Basket
     (
-        certification_id      INT            NOT NULL    COMMENT '인증서 아이디', 
-        number_times  INT            NULL        COMMENT '대여 횟수', 
-        responsible   VARCHAR(45)    NULL        COMMENT '책임자', 
-        code          VARCHAR(45)    NULL        COMMENT '일련번호', 
-        CONSTRAINT PK_TB_certification PRIMARY KEY (certification_id)
+        basket_id   INT    NOT NULL    AUTO_INCREMENT COMMENT '장바구니 아이디', 
+        product_id  INT    NULL        COMMENT '제품 아이디', 
+        CONSTRAINT PK_TB_Basket PRIMARY KEY (basket_id),
+        CONSTRAINT FK_TB_Products_product_id_TB_Basket_product_id FOREIGN KEY(product_id)
+        REFERENCES TB_Products(product_id)
     );
     `,
     (err, results, fields) => {
@@ -122,7 +147,9 @@ db.getConnection((err, conn) => {
         product_id  INT     NULL        COMMENT '제품 아이디', 
         rent_Date   INT     NULL        COMMENT '대여 일수', 
         rent_state  ENUM('HIGH', 'MIDDLE', 'LOW')    NULL        COMMENT '대여 상태', 
-        CONSTRAINT PK_TB_Record PRIMARY KEY (record_id)
+        CONSTRAINT PK_TB_Record PRIMARY KEY (record_id),
+        CONSTRAINT FK_TB_Products_product_id_TB_Record_product_id FOREIGN KEY(product_id)
+        REFERENCES TB_Products(product_id)
     );
     `,
     (err, results, fields) => {
@@ -138,7 +165,9 @@ db.getConnection((err, conn) => {
         product_id  INT             NULL        COMMENT '제품 아이디', 
         image_main  VARCHAR(100)    NULL        COMMENT '파일명', 
         image_type  ENUM('PRODUCT', 'MODEL')           NULL        COMMENT '사진유형', 
-        CONSTRAINT PK_TB_Image PRIMARY KEY (image_id)
+        CONSTRAINT PK_TB_Image PRIMARY KEY (image_id),
+        CONSTRAINT FK_TB_Products_product_id_TB_Image_product_id FOREIGN KEY(product_id)
+        REFERENCES TB_Products(product_id)
     );
     `,
     (err, results, fields) => {
@@ -160,33 +189,6 @@ db.getConnection((err, conn) => {
       if (err) throw err;
     }
   );
-
-  conn.query(
-    `  
-    CREATE TABLE IF NOT EXISTS TB_manufacture
-    (
-        manufacture_id    INT            NOT NULL    AUTO_INCREMENT COMMENT '제품 제조사 아이디', 
-        manufacture_name  VARCHAR(45)    NULL        COMMENT '제조사 이름', 
-        manufacture_adr   VARCHAR(45)    NULL        COMMENT '제조사 주소', 
-        CONSTRAINT PK_TB_manufacture PRIMARY KEY (manufacture_id)
-    );
-    `,
-    (err, results, fields) => {
-      if (err) throw err;
-    }
-  );
-
-  // conn.query(
-  //   `
-  //     ALTER TABLE TB_Info
-  //       ADD CONSTRAINT FK_TB_Info_info_id_TB_Products_info_id FOREIGN KEY (info_id)
-  //         REFERENCES TB_Products (info_id) ON DELETE CASCADE
-  //   `,
-  //   (err, results) => {
-  //     if (err) throw err;
-  //     console.log(results);
-  //   }
-  // );
 });
 
 module.exports = db;
