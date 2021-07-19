1468b56fd2e84265b72129050075e48522dd0c0b
bellDev-code <sumaoo20@naver.com>
2021-07-19 22:50:04 +0900

    query Question

--- back-end/src/routers/api.js
+++ back-end/src/routers/api.js
@@ -3,8 +3,14 @@ const db = require("../db");
 
 const router = express.Router();
 
-// status code 200대 = 정상
-// status code 400대 = error
+// 쿼리문 구조
+// 제품 리스트 페이지에서 보여질 "/products"
+// 상품 디테일 페이지(TB_Products - TB_Info - TB_Manufacture) 표현 어떻게?
+// 결제창 (TB_Payment)
+// 인증서 및 대여기록(rent_date, rent_state)은 지금 상황에서 데이터 보여주는것만
+// TB_Payment에서 start_rent, end_rent 등 데이터 받는 방법
+// TB_Record에서 rent_date에서 데이터 어떻게 받는지,
+// rent_state data type('렌트가능', '렌트중')이 맞는지
 router.get("/products", (req, res, next) => {
   db.getConnection((err, conn) => {
     if (err) return res.status(403);
@@ -15,9 +21,6 @@ router.get("/products", (req, res, next) => {
         product.product_brand,
         product.product_color,
         product.rent_price,
-        info.info_id,
-        info.info_gender,
-        info.info_material,
         image.image_id,
         image.image_main,
         image.image_type,
@@ -27,7 +30,6 @@ router.get("/products", (req, res, next) => {
         productGroup.number_stock,
         productGroup.number_rent
         FROM TB_Products AS product
-        LEFT JOIN TB_Info AS info ON product.info_id = info.info_id
         LEFT JOIN TB_Image AS image ON product.product_id = image.product_id
         LEFT JOIN TB_ProductGroup AS productGroup ON product.group_id = productGroup.group_id
       `,
