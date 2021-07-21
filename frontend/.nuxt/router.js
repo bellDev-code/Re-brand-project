fd8dafbd0f7cb90a3f4d1a1f796961d53b4f523c
PastelWhale <aviate8@gmail.com>
2021-07-21 09:34:04 +0900

    router

--- frontend/.nuxt/router.js
+++ frontend/.nuxt/router.js
@@ -4,27 +4,25 @@ import { normalizeURL, decode } from 'ufo'
 import { interopDefault } from './utils'
 import scrollBehavior from './router.scrollBehavior.js'
 
-const _3131ffbc = () => interopDefault(import('..\\pages\\about-us.vue' /* webpackChunkName: "pages/about-us" */))
-const _34adcf3a = () => interopDefault(import('..\\pages\\about-us copy.vue' /* webpackChunkName: "pages/about-us copy" */))
-const _07d58782 = () => interopDefault(import('..\\pages\\faq.vue' /* webpackChunkName: "pages/faq" */))
-const _15f21a32 = () => interopDefault(import('..\\pages\\my-account\\index.vue' /* webpackChunkName: "pages/my-account/index" */))
-const _3ba33e4a = () => interopDefault(import('..\\pages\\privacy-policy.vue' /* webpackChunkName: "pages/privacy-policy" */))
-const _bc7816c0 = () => interopDefault(import('..\\pages\\blockchain\\guarantee.vue' /* webpackChunkName: "pages/blockchain/guarantee" */))
-const _f470922e = () => interopDefault(import('..\\pages\\blockchain\\record.vue' /* webpackChunkName: "pages/blockchain/record" */))
-const _7ef3bdf8 = () => interopDefault(import('..\\pages\\cart\\cart.vue' /* webpackChunkName: "pages/cart/cart" */))
-const _fd06f400 = () => interopDefault(import('..\\pages\\cart\\cart_copy.vue' /* webpackChunkName: "pages/cart/cart_copy" */))
-const _19531c84 = () => interopDefault(import('..\\pages\\cart\\empty-cart.vue' /* webpackChunkName: "pages/cart/empty-cart" */))
-const _2a406102 = () => interopDefault(import('..\\pages\\my-account\\addresses.vue' /* webpackChunkName: "pages/my-account/addresses" */))
-const _1f1c8016 = () => interopDefault(import('..\\pages\\my-account\\checkout.vue' /* webpackChunkName: "pages/my-account/checkout" */))
-const _34ef1227 = () => interopDefault(import('..\\pages\\my-account\\checkout-cart.vue' /* webpackChunkName: "pages/my-account/checkout-cart" */))
-const _3ab461cb = () => interopDefault(import('..\\pages\\my-account\\downloads.vue' /* webpackChunkName: "pages/my-account/downloads" */))
-const _6119c0e8 = () => interopDefault(import('..\\pages\\my-account\\guarantee.vue' /* webpackChunkName: "pages/my-account/guarantee" */))
-const _a6ebc796 = () => interopDefault(import('..\\pages\\my-account\\orders.vue' /* webpackChunkName: "pages/my-account/orders" */))
-const _f06ca7bc = () => interopDefault(import('..\\pages\\my-account\\payment_complete.vue' /* webpackChunkName: "pages/my-account/payment_complete" */))
-const _9362ddd6 = () => interopDefault(import('..\\pages\\my-account\\wishlist.vue' /* webpackChunkName: "pages/my-account/wishlist" */))
-const _1555efa8 = () => interopDefault(import('..\\pages\\shop\\shopfilter.vue' /* webpackChunkName: "pages/shop/shopfilter" */))
-const _2ed5a802 = () => interopDefault(import('..\\pages\\product\\_slug.vue' /* webpackChunkName: "pages/product/_slug" */))
-const _19f4023e = () => interopDefault(import('..\\pages\\index.vue' /* webpackChunkName: "pages/index" */))
+const _64941503 = () => interopDefault(import('..\\pages\\about-us.vue' /* webpackChunkName: "pages/about-us" */))
+const _3d7ae822 = () => interopDefault(import('..\\pages\\about-us copy.vue' /* webpackChunkName: "pages/about-us copy" */))
+const _7bd52a01 = () => interopDefault(import('..\\pages\\faq.vue' /* webpackChunkName: "pages/faq" */))
+const _e6701b1e = () => interopDefault(import('..\\pages\\my-account\\index.vue' /* webpackChunkName: "pages/my-account/index" */))
+const _f6d2ee08 = () => interopDefault(import('..\\pages\\privacy-policy.vue' /* webpackChunkName: "pages/privacy-policy" */))
+const _e534d542 = () => interopDefault(import('..\\pages\\blockchain\\guarantee.vue' /* webpackChunkName: "pages/blockchain/guarantee" */))
+const _2ca632ec = () => interopDefault(import('..\\pages\\blockchain\\record.vue' /* webpackChunkName: "pages/blockchain/record" */))
+const _b27cfcb6 = () => interopDefault(import('..\\pages\\cart\\cart.vue' /* webpackChunkName: "pages/cart/cart" */))
+const _7b5c7f36 = () => interopDefault(import('..\\pages\\cart\\empty-cart.vue' /* webpackChunkName: "pages/cart/empty-cart" */))
+const _15e201c1 = () => interopDefault(import('..\\pages\\my-account\\addresses.vue' /* webpackChunkName: "pages/my-account/addresses" */))
+const _1e744b77 = () => interopDefault(import('..\\pages\\my-account\\checkout.vue' /* webpackChunkName: "pages/my-account/checkout" */))
+const _41443b66 = () => interopDefault(import('..\\pages\\my-account\\checkout-cart.vue' /* webpackChunkName: "pages/my-account/checkout-cart" */))
+const _2656028a = () => interopDefault(import('..\\pages\\my-account\\downloads.vue' /* webpackChunkName: "pages/my-account/downloads" */))
+const _106f4bd6 = () => interopDefault(import('..\\pages\\my-account\\orders.vue' /* webpackChunkName: "pages/my-account/orders" */))
+const _9bd6f2fa = () => interopDefault(import('..\\pages\\my-account\\payment_complete.vue' /* webpackChunkName: "pages/my-account/payment_complete" */))
+const _94b34714 = () => interopDefault(import('..\\pages\\my-account\\wishlist.vue' /* webpackChunkName: "pages/my-account/wishlist" */))
+const _8356d8ee = () => interopDefault(import('..\\pages\\shop\\shopfilter.vue' /* webpackChunkName: "pages/shop/shopfilter" */))
+const _ea0557c0 = () => interopDefault(import('..\\pages\\product\\_slug.vue' /* webpackChunkName: "pages/product/_slug" */))
+const _e6d5fe06 = () => interopDefault(import('..\\pages\\index.vue' /* webpackChunkName: "pages/index" */))
 
 const emptyFn = () => {}
 
@@ -39,87 +37,79 @@ export const routerOptions = {
 
   routes: [{
     path: "/about-us",
-    component: _3131ffbc,
+    component: _64941503,
     name: "about-us"
   }, {
     path: "/about-us%20copy",
-    component: _34adcf3a,
+    component: _3d7ae822,
     name: "about-us copy"
   }, {
     path: "/faq",
-    component: _07d58782,
+    component: _7bd52a01,
     name: "faq"
   }, {
     path: "/my-account",
-    component: _15f21a32,
+    component: _e6701b1e,
     name: "my-account"
   }, {
     path: "/privacy-policy",
-    component: _3ba33e4a,
+    component: _f6d2ee08,
     name: "privacy-policy"
   }, {
     path: "/blockchain/guarantee",
-    component: _bc7816c0,
+    component: _e534d542,
     name: "blockchain-guarantee"
   }, {
     path: "/blockchain/record",
-    component: _f470922e,
+    component: _2ca632ec,
     name: "blockchain-record"
   }, {
     path: "/cart/cart",
-    component: _7ef3bdf8,
+    component: _b27cfcb6,
     name: "cart-cart"
   }, {
-    path: "/cart/cart_copy",
-    component: _fd06f400,
-    name: "cart-cart_copy"
-  }, {
     path: "/cart/empty-cart",
-    component: _19531c84,
+    component: _7b5c7f36,
     name: "cart-empty-cart"
   }, {
     path: "/my-account/addresses",
-    component: _2a406102,
+    component: _15e201c1,
     name: "my-account-addresses"
   }, {
     path: "/my-account/checkout",
-    component: _1f1c8016,
+    component: _1e744b77,
     name: "my-account-checkout"
   }, {
     path: "/my-account/checkout-cart",
-    component: _34ef1227,
+    component: _41443b66,
     name: "my-account-checkout-cart"
   }, {
     path: "/my-account/downloads",
-    component: _3ab461cb,
+    component: _2656028a,
     name: "my-account-downloads"
   }, {
-    path: "/my-account/guarantee",
-    component: _6119c0e8,
-    name: "my-account-guarantee"
-  }, {
     path: "/my-account/orders",
-    component: _a6ebc796,
+    component: _106f4bd6,
     name: "my-account-orders"
   }, {
     path: "/my-account/payment_complete",
-    component: _f06ca7bc,
+    component: _9bd6f2fa,
     name: "my-account-payment_complete"
   }, {
     path: "/my-account/wishlist",
-    component: _9362ddd6,
+    component: _94b34714,
     name: "my-account-wishlist"
   }, {
     path: "/shop/shopfilter",
-    component: _1555efa8,
+    component: _8356d8ee,
     name: "shop-shopfilter"
   }, {
     path: "/product/:slug?",
-    component: _2ed5a802,
+    component: _ea0557c0,
     name: "product-slug"
   }, {
     path: "/",
-    component: _19f4023e,
+    component: _e6d5fe06,
     name: "index"
   }],
 
