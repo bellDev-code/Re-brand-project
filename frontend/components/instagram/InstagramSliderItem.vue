5e89072d1b024c6b2b65f7f57b76cb5f5b8aeb9c
Heekyung Do <dhk0729@gmail.com>
2021-07-21 16:00:16 +0900

    insta

--- frontend/components/instagram/InstagramSliderItem.vue
+++ frontend/components/instagram/InstagramSliderItem.vue
@@ -1,21 +1,20 @@
 <template>
-   <div class="instgram_post">
-        <a href="#!">
-            <i class="fab fa-instagram"></i>
-            <img :src="instagramImg" alt="img" />
-        </a>
-    </div>
+  <div class="instgram_post">
+    <a :href="instagramURL">
+      <i class="fab fa-instagram"></i>
+      <img :src="instagramImg" alt="img"
+    /></a>
+  </div>
 </template>
 
 <script>
 export default {
-    name: 'instagram-slider-item',
-    props: {
-        instagramImg: String
-    }
-}
+  name: "instagram-slider-item",
+  props: {
+    instagramImg: String,
+    instagramURL: String,
+  },
+};
 </script>
 
-<style>
-
-</style>
+<style></style>
